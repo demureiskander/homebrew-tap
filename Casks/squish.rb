7ebea86b3c850a1878cb79eacc92a66733d41658
Demure Iskander <147938590+demureiskander@users.noreply.github.com>
2026-07-23 14:32:42 +0300

    Fix macOS dependency version for Squish

--- Casks/squish.rb
+++ Casks/squish.rb
@@ -7,7 +7,7 @@ cask "squish" do
   desc "Image compressor, converter and resizer for macOS"
   homepage "https://github.com/demureiskander/squish"
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: ":sonoma"
 
   app "Squish.app"
 
